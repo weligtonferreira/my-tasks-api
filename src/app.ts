43b8309cb1af145360bd19fe70a73880c0027b75
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-01 15:41:58 -0300

    feat(app): configure handlers

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import * as dotenv from 'dotenv';
 import path from 'path';
 
+import { handleExceptions } from './middlewares/handleExceptions';
+
 dotenv.config({ path: path.resolve() + '/.env.local' });
 
 class App {
@@ -11,11 +13,16 @@ class App {
   constructor() {
     this.app = express();
     this.middlewares();
+    this.handlers();
   }
 
   middlewares() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  handlers() {
+    this.app.use(handleExceptions);
   }
 }
 
