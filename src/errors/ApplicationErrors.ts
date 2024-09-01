6f353a6dea7ad0bdb7183f9203a164e1b326c54b
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-01 15:35:47 -0300

    feat: add ApplicationErrors class

---
+++ src/errors/ApplicationErrors.ts
@@ -0,0 +1,9 @@
+export class ApplicationErrors {
+  readonly message: string;
+  readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
