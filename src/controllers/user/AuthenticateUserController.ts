b403c51186ffd0c91844093f362c4474cf213723
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-02 11:31:21 -0300

    feat: add AuthenticateUserController class

---
+++ src/controllers/user/AuthenticateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { ILoginCredentialsRequest } from '../../dto/user/ILoginCredentialsRequest';
+import AuthenticateUserService from '../../services/user/AuthenticateUserService';
+
+class AuthenticateUserController {
+  async handle(req: Request, res: Response) {
+    const { email, password } = req.body as ILoginCredentialsRequest;
+
+    const token = await AuthenticateUserService.execute({ email, password });
+
+    return res.status(201).json({ accessToken: token });
+  }
+}
+
+export default new AuthenticateUserController();
