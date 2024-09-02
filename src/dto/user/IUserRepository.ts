783c4f2560eca54e3d09b167cf16f5a54a98e542
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-02 11:05:11 -0300

    refactor(IUserRepository): change findById return

--- src/dto/user/IUserRepository.ts
+++ src/dto/user/IUserRepository.ts
@@ -2,10 +2,11 @@ import { User } from '@prisma/client';
 
 import { ICreateUserRequest } from './ICreateUserRequest';
 import { IUpdateUserRequest } from './IUpdateUserRequest';
+import { IUserResponse } from './IUserResponse';
 
 export interface IUserRepository {
   create: (params: ICreateUserRequest) => Promise<User | null>;
-  findById: (id: string) => Promise<User | null>;
+  findById: (id: string) => Promise<IUserResponse | null>;
   findByEmail: (email: string) => Promise<User | null>;
   findAll: () => Promise<User[] | null>;
   updateById: (id: string, params: IUpdateUserRequest) => Promise<void>;
