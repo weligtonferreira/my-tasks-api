3adc45b89663114c97393966c02edf7089a77a51
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-02 11:55:01 -0300

    feat(userRoutes): add authenticate user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,9 +5,11 @@ import ListUserController from '../controllers/user/ListUserController';
 import CreateUserController from '../controllers/user/CreateUserController';
 import DeleteUserController from '../controllers/user/DeleteUserController';
 import UpdateUserController from '../controllers/user/UpdateUserController';
+import AuthenticateUserController from '../controllers/user/AuthenticateUserController';
 
 const router = Router();
 
+router.post('/login', AuthenticateUserController.handle);
 router.get('/', ListUsersController.handle);
 router.get('/:id', ListUserController.handle);
 router.post('/', CreateUserController.handle);
