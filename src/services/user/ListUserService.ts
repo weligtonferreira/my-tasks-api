bdce96aed0d5870d9adedbbc5f88a003ade0da22
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-02 11:16:00 -0300

    refactor(ListUserService): change return of execute function

--- src/services/user/ListUserService.ts
+++ src/services/user/ListUserService.ts
@@ -1,10 +1,9 @@
-import { User } from '@prisma/client';
-
 import UserRepository from '../../repositories/UserRepository';
 import { ApplicationErrors } from '../../errors/ApplicationErrors';
+import { IUserResponse } from '../../dto/user/IUserResponse';
 
 class ListUserService {
-  async execute(id: string): Promise<User | null> {
+  async execute(id: string): Promise<IUserResponse | null> {
     const user = await UserRepository.findById(id);
 
     if (!user) throw new ApplicationErrors('User not found', 404);
