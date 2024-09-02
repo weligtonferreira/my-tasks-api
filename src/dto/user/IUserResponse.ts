c489860b69065338ff7d86b4f13da439a116cdfc
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-02 11:02:14 -0300

    feat: add IUserResponse interface

---
+++ src/dto/user/IUserResponse.ts
@@ -0,0 +1,11 @@
+import { Task } from '@prisma/client';
+
+import { ITasksCount } from '../task/ITaskCount';
+
+export interface IUserResponse {
+  id: string;
+  name: string;
+  email: string;
+  _count: ITasksCount;
+  tasks: Task[];
+}
