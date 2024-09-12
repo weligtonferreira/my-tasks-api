903b0a46e8fe01ac1d6a4aefe5e89b0352055684
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-12 14:20:45 -0300

    feat: add ITaskRepository interface

---
+++ src/dto/task/ITaskRepository.ts
@@ -0,0 +1,19 @@
+import { Task } from '@prisma/client';
+
+import { ICreateTaskRequest } from './ICreateTaskRequest';
+import { IUpdateTaskRequest } from './IUpdateTaskRequest';
+import { ITaskResponse } from './ITaskResponse';
+
+export interface ITaskRepository {
+  create: (
+    userId: string,
+    createTaskInputData: ICreateTaskRequest
+  ) => Promise<Task | null>;
+  findById: (id: string) => Promise<ITaskResponse | null>;
+  findAll: () => Promise<Task[] | null>;
+  updateById: (
+    id: string,
+    updateTaskInputData: IUpdateTaskRequest
+  ) => Promise<void>;
+  deleteById: (id: string) => Promise<void>;
+}
