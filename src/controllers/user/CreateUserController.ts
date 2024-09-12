28992f437be0c8271ea8878d409d8f8acabd04c0
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-12 13:58:02 -0300

    refactor(CreateUserController): change the way data is passed to the execute function

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -5,9 +5,9 @@ import CreateUserService from '../../services/user/CreateUserService';
 
 class CreateUserController {
   async handle(req: Request, res: Response) {
-    const { name, email, password }: ICreateUserRequest = req.body;
+    const createUserInputData: ICreateUserRequest = req.body;
 
-    const user = await CreateUserService.execute({ name, email, password });
+    const user = await CreateUserService.execute(createUserInputData);
 
     return res.status(201).json(user);
   }
