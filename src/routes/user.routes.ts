9f5650b916bc088ea0c9d21e7c948d581159ea33
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-15 11:14:37 -0300

    feat: add verifyJwtToken to some user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { verifyJwtToken } from '../middlewares/verifyJwtToken';
+
 import ListUsersController from '../controllers/user/ListUsersController';
 import ListUserController from '../controllers/user/ListUserController';
 import CreateUserController from '../controllers/user/CreateUserController';
@@ -10,10 +12,10 @@ import AuthenticateUserController from '../controllers/user/AuthenticateUserCont
 const router = Router();
 
 router.post('/login', AuthenticateUserController.handle);
-router.get('/', ListUsersController.handle);
-router.get('/:id', ListUserController.handle);
+router.get('/', verifyJwtToken, ListUsersController.handle);
+router.get('/:id', verifyJwtToken, ListUserController.handle);
 router.post('/', CreateUserController.handle);
-router.put('/:id', UpdateUserController.handle);
-router.delete('/:id', DeleteUserController.handle);
+router.put('/:id', verifyJwtToken, UpdateUserController.handle);
+router.delete('/:id', verifyJwtToken, DeleteUserController.handle);
 
 export default router;
